4b25e5b2c85b815a324efc19386c83cc950b4439
LyudmilaDr <ludralo44@gmail.com>
2023-03-20 18:24:30 +0200

    add fix styled

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,24 +1,33 @@
 import PropTypes from 'prop-types';
-import {FriendData, FriendItem, FriendSpan} from './FriendList.styled';
+import { FriendData, FriendItem, FriendStatus } from './FriendList.styled';
 
-export const FriendList = ({friends}) => {
-    return (<FriendData >
-    {friends.map(({avatar, name, isOnline, id}) => {
-        return (<FriendItem key = {id}>
-        <FriendSpan>{isOnline}</FriendSpan>
-        <img  src={avatar} alt="User avatar" width="48" />
-        <p>{name}</p>
-      </FriendItem>)
-})}
-  </FriendData>)}
+export const FriendList = ({ friends }) => {
+  return (
+    <FriendData>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendItem key={id}>
+            <FriendStatus
+              style={{
+                backgroundColor: isOnline ? 'green' : 'red',
+              }}
+            />
+            <img src={avatar} alt="User avatar" width="48" />
+            <p>{name}</p>
+          </FriendItem>
+        );
+      })}
+    </FriendData>
+  );
+};
 
 FriendList.propTypes = {
-  friends:PropTypes.arrayOf(
+  friends: PropTypes.arrayOf(
     PropTypes.shape({
-      avatar:PropTypes.string.isRequired,
-      name:PropTypes.string.isRequired,
-      isOnline:PropTypes.bool.isRequired,
-      id:PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+      id: PropTypes.number.isRequired,
     })
   ),
 };
--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -1,33 +1,27 @@
 import styled from 'styled-components';
 export const FriendData = styled.ul`
-display: flex;
-flex-direction: column;
-align-items: center;
-gap: 10px;
-width: 640px;
-margin-bottom: 30px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  width: 640px;
+  margin-bottom: 30px;
+`;
 
 export const FriendItem = styled.li`
-display: flex;
-align-items: center;
-justify-content: space-around;
-width: 300px;
-padding: 10px;
-gap: 20px;
-border-radius: 5px;
-box-shadow: 1px 4px 6px rgba(0, 0, 0, 0.16),
-        1px 4px 4px rgba(0, 0, 0, 0.06),
-        1px 1px 1px rgba(0, 0, 0, 0.12);`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 300px;
+  padding: 10px;
+  gap: 20px;
+  border-radius: 5px;
+  box-shadow: 1px 4px 6px rgba(0, 0, 0, 0.16), 1px 4px 4px rgba(0, 0, 0, 0.06),
+    1px 1px 1px rgba(0, 0, 0, 0.12);
+`;
 
- export const FriendSpan = styled.span`
- width: 20px;
- height: 20px;
- border-radius: 50%;
- background-color: ${props => {
-        if(!props.value){
-                return 'red';
-        } else {
-return 'green';
-        }
- }}`
+export const FriendStatus = styled.span`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+`;
